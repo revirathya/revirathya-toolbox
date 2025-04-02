2947349144d8f6686f375cc7ea42443d32f7f6fc
avidito <av.leonardif@gmail.com>
2025-04-02 22:43:41 +0700

    faet: shorten lib import naming

--- src/revi_toolbox/scraper/manga/__init__.py
+++ src/revi_toolbox/scraper/manga/__init__.py
@@ -1,1 +1,8 @@
+# Base
+from revi_toolbox.scraper.manga.base import MangaScraperRunner
+
+# Schemas
+from revi_toolbox.scraper.manga.schemas import MangaChapter, MangaOverview
+
+# Runner
 from revi_toolbox.scraper.manga.runner.mangabats import MangabatsScraperRunner
